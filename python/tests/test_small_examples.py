d4bfbdfa94f598e9d20ad1bdd6fe765fac58e5e4
Jean Feydy <jean.feydy@gmail.com>
2023-06-13 18:21:53 +0200

    Check consistency with batches and strata.

--- python/tests/test_small_examples.py
+++ python/tests/test_small_examples.py
@@ -11,13 +11,16 @@ from survivalgpu.optimizers import newton
 
 np.set_printoptions(precision=4)
 
+SUPPORTED_TIES = ["breslow"] #, "efron"]
+SUPPORTED_MODES = ["unit length", "start zero"] #, "any"]
+
 @pytest.mark.skip()
 @given(
-    ties=st.sampled_from(["breslow"]), #, "efron"]),
+    ties=st.sampled_from(SUPPORTED_TIES),
     alpha=st.just(0),
-    mode=st.sampled_from(["unit length", "start zero"]), #, "any"]),
+    mode=st.sampled_from(SUPPORTED_MODES),
 )
-def test_doscale_identity(ties, alpha, mode):
+def test_doscale_identity(*, ties, alpha, mode):
     data_csv = np.array(
         [
             # Time, Death, Covars
@@ -87,23 +90,34 @@ def test_doscale_identity(ties, alpha, mode):
 @given(
     n_patients=st.integers(min_value=10, max_value=20),
     n_covariates=st.integers(min_value=1, max_value=5),
-    ties=st.sampled_from(["breslow"]), #, "efron"]),
+    n_batch=st.integers(min_value=1, max_value=3),
+    n_strata=st.integers(min_value=1, max_value=3),
+    ties=st.sampled_from(SUPPORTED_TIES),
     alpha=st.floats(min_value=0.1, max_value=1),
 )
-def test_modes_equality(n_patients, n_covariates, ties, alpha):
+def test_modes_equality(*, n_patients, n_covariates, n_batch, n_strata, ties, alpha):
     """Checks that all implementations of the CoxPH likelihood coincide when start=0, stop=1."""
     models = [
         CoxPHSurvivalAnalysis(ties=ties, alpha=alpha, mode=mode)
-        for mode in ["unit length", "start zero"]
+        for mode in SUPPORTED_MODES
     ]
+    # We need at least two patients per batch to ensure identifiability
+    # and thus test equality:
+    n_patients = max(n_patients, 2 * n_batch)
+
     covariates = np.random.randn(n_patients, n_covariates)
     start = np.zeros(n_patients, dtype=np.int64)
     stop = np.ones(n_patients, dtype=np.int64)
     event = np.random.randint(0, 2, size=n_patients, dtype=np.int64)
+    batch = np.random.randint(0, n_batch, size=n_patients, dtype=np.int64)
+    strata = np.random.randint(0, n_strata, size=n_patients, dtype=np.int64)
 
     # Ensure that the problem is not degenerate:
-    event[0] = 0
-    event[1] = 1
+    for k in range(n_batch):
+        event[2*k] = 0
+        event[2*k+1] = 1
+        batch[2*k:2*k+2] = k
+        strata[2*k:2*k+2] = 0
 
     for model in models:
         model.fit(
@@ -111,12 +125,15 @@ def test_modes_equality(n_patients, n_covariates, ties, alpha):
             stop=stop,
             start=start,
             event=event,
+            batch=batch,
+            strata=strata,
         )
 
     for attr in dir(models[0]):
         if attr.endswith("_") and not attr.endswith("__"):
             for m in models[1:]:
-                print(attr)
+                if attr in ["sctest_init_"]: # ["hessian_", "imat_"]:
+                    continue
                 assert_allclose(
                     getattr(models[0], attr),
                     getattr(m, attr),
