383607f7e12fde667ffaf5062a6c24d17b2c322e
Antoine Poirot-Bourdain <antoine.poirot.bourdain@gmail.com>
2024-01-30 15:50:00 +0100

    added simulation times to json

--- simulations/simulate_WCEmat.r
+++ simulations/simulate_WCEmat.r
@@ -53,17 +53,25 @@ for (n_patients in n_patients_list){
 
     Xmat <- generate_Xmat(observation_time,n_patients,doses)
     write.csv(Xmat, paste0(path_experiement_result,"/Xmat/", n_patients,".csv  "))
+    simulation_time_list <- list()
     
 
     for (scenario in scenario_list){
-        print(paste("#### Generating scenario : ",scenario))
-        print(paste("##n_patients = ",n_patients))
+        
+        start_time <- Sys.time()
         wce_mat <- do.call("rbind", lapply(1:dim(Xmat)[1], wce_vector, scenario = scenario_translator(scenario), Xmat = Xmat,normalization = normalization))
         df_wce <- get_dataset(Xmat = Xmat, wce_mat = wce_mat)
         export_path <- paste0(path_experiement_result,"/WCEmat/", scenario,"_",n_patients,".csv")
         print(export_path)
         write.csv(df_wce, export_path, row.names=FALSE) 
+        end_time <- Sys.time()
+        simulation_time <- end_time - start_time
+        scenario_time <- list(scenario = simulation_time)
         }
+    simulation_time_list[[n_patients]] <-scenario_time
+    json_file_name <-paste0(path_experiement_result,"simulation_time.json")
+    write(toJSON(simulation_time_list), file = json_file_name)
+
  }
 
 
