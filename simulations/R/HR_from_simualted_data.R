c0d9aa00ad3289122e11a575193f3703060c43e5
Antoine Poirot-Bourdain <antoine.poirot.bourdaingmail.com>
2024-02-29 17:01:31 +0100

    HR modeified

--- simulations/R/HR_from_simualted_data.R
+++ simulations/R/HR_from_simualted_data.R
@@ -3,35 +3,36 @@ library(boot)
 library(jsonlite)
 library(devtools)
 
+options(scipen = 999)
+
 devtools::load_all("../../../survivalGPU/R")
 
-##################### EXPERIMENTS DATA #####################
+########################################## EXPERIMENTS DATA ##########################################
 
-expriment_name = "simple_test"
+expriment_name = "HR : 100 - 100 000"
 
 # static parameters 
-n_bootstraps = 10
+n_bootstraps = 1000
 cutoff = 180
 
 # variable paramters 
-HR_cible_list = c(2.8)
+HR_target_list = c(1.5)
 weight_functions_list = c("exponential_weight")
-n_patients_list = c(100,1000)
-n_knots_list = c(1,2)
+n_patients_list = c(100,1000,10000)
+n_knots_list = c(1)
+
+
+######################################################################################################
 
 
 
-###########################################################
-
-
-
-analyze_data <- function(file_path, cutoff, n_bootstraps,nknots) {
+analyze_data <- function(file_path, cutoff, n_bootstraps,n_knots, batchsize) {
 
     data = read.csv(file_path)
 
-    model <- wceGPU(data, nknots, cutoff, constrained = "right",
+    model <- wceGPU(data, n_knots, cutoff, constrained = "right",
                id = "patient", event = "event", start = "start",
-               stop = "stop", expos = "dose",nbootstraps = n_bootstraps,batchsize = 10, verbosity=0)
+               stop = "stop", expos = "dose",nbootstraps = n_bootstraps,batchsize = batchsize, verbosity=0)
 
     exposed   <- rep(1, cutoff)
     non_exposed   <- rep(0, cutoff)
@@ -40,11 +41,10 @@ analyze_data <- function(file_path, cutoff, n_bootstraps,nknots) {
 }
 
 
-file_name_translator <- function(n_patients, HR_cible,weight_function){
+file_name_translator <- function(n_patients, HR_target,weight_function){
 
-    HR_cible = 1 ## WILL CHANGE WHEN SIMULATION IMPLEMENT HR 
-    file_name = paste0(weight_function,"_",as.character(HR_cible),"_",as.character(n_patients),".csv")
-    file_path = file.path("../WCEmat",file_name)
+    file_name = paste0(weight_function,"_",as.character(n_patients),".csv")
+    file_path = file.path("../WCEmat",paste0("HR-",as.character(HR_target)),file_name)
 
 
     return(file_path)
@@ -59,25 +59,35 @@ weight_function_results = c()
 n_patients_results = c()
 n_bootstraps_results = c()
 cutoff_results = c()
-HR_cible_results = c()
+HR_target_results = c()
 HR_calculated_results = c()
 HR_calculated_2_5_results = c()
 HR_calculated_97_5_results = c()
 
-combinaisons_parameters <- expand.grid(HR = HR_cible_list,weight_function = weight_functions_list, n_patients = n_patients_list,n_knots= n_knots_list)
+combinaisons_parameters <- expand.grid(HR_target = HR_target_list,weight_function = weight_functions_list, n_patients = n_patients_list,n_knots= n_knots_list)
 print(combinaisons_parameters)
 
 for (i in 1:nrow(combinaisons_parameters)){
 
-    HR_cible <- combinaisons_parameters$HR[i]
+    HR_target <- combinaisons_parameters$HR_target[i]
     weight_function <- combinaisons_parameters$weight_function[i]
     n_patients <- combinaisons_parameters$n_patients[i]
-    nknots <- combinaisons_parameters$n_knots[i]
+    n_knots <- combinaisons_parameters$n_knots[i]
+
+    print(paste0("Starting computation for :" ,as.character(n_patients),"patients - HR_target = ",HR_target," - weight function = ",weight_function,"- ",as.character(n_knots)," knots"))
 
 
-    file_path = file_name_translator(n_patients,HR_cible,weight_function)
+    file_path = file_name_translator(n_patients,HR_target,weight_function)
     print(file_path)
-    HR_result = analyze_data(file_path, cutoff,n_bootstraps,nknots)
+
+
+    if (n_patients < 10000){
+        batchsize = 100
+    }else{
+        batchsize = 10
+    }
+
+    HR_result = analyze_data(file_path, cutoff,n_bootstraps,n_knots,batchsize)
     print(HR_result)
 
     ### here logic is false, see https://datatofish.com/export-dataframe-to-csv-in-r/ for better
@@ -87,7 +97,7 @@ for (i in 1:nrow(combinaisons_parameters)){
     n_patients_results = c(n_patients_results,n_patients)  
     n_bootstraps_results = c(n_bootstraps_results,n_bootstraps)  
     cutoff_results = c(cutoff_results,cutoff)  
-    HR_cible_results = c(HR_cible_results,HR_cible)       
+    HR_target_results = c(HR_target_results,HR_target)       
     HR_calculated_results = c(HR_calculated_results,HR_result[1])  
     HR_calculated_2_5_results = c(HR_calculated_2_5_results,HR_result[2]) 
     HR_calculated_97_5_results = c(HR_calculated_97_5_results,HR_result[3])  
@@ -97,7 +107,7 @@ result_dict = list("weight_function"= weight_function_results,
                    "n_patients"= n_patients_results,
                    "n_bootstraps"= n_bootstraps_results,
                    "cutoff"= cutoff_results,
-                   "HR_cible"= HR_cible_results,
+                   "HR_target"= HR_target_results,
                    "HR_calculated"= HR_calculated_results,
                    "HR_calculated_2_5"= HR_calculated_2_5_results,
                    "HR_calculated_97_5"= HR_calculated_97_5_results
