aa9a1745802005e5cb1e83331d0393ea4f88bc0b
Antoine Poirot-Bourdain <antoine.poirot.bourdain@gmail.com>
2024-03-04 11:26:24 +0100

    save image correctly

--- simulations/R/HR_from_simualted_data.R
+++ simulations/R/HR_from_simualted_data.R
@@ -14,16 +14,16 @@ source("../src/weight_functions.r")
 
 ########################################## EXPERIMENTS DATA ##########################################
 
-expriment_name = "HR : 100 - 100 000"
+expriment_name = "test_images"
 
 # static parameters 
-n_bootstraps = 1000
+n_bootstraps = 100
 cutoff = 180
 
 # variable paramters 
 HR_target_list = c(3)
 weight_functions_list = c("exponential_weight") #c("exponential_weight")
-n_patients_list = c(1000)#,10000)
+n_patients_list = c(100)#,10000)
 n_knots_list = c(1)
 
 
@@ -53,6 +53,13 @@ HR_CPU_resutls = c()
 combinaisons_parameters <- expand.grid(HR_target = HR_target_list,weight_function = weight_functions_list, n_patients = n_patients_list,n_knots= n_knots_list)
 print(combinaisons_parameters)
 
+result_dict_path = file.path("../Simulation_results",expriment_name)
+
+if (!dir.exists(result_dict_path)){
+dir.create(result_dict_path)
+} 
+
+
 for (i in 1:nrow(combinaisons_parameters)){
 
     HR_target <- combinaisons_parameters$HR_target[i]
@@ -81,14 +88,14 @@ for (i in 1:nrow(combinaisons_parameters)){
 
 
             
-    wce_model_GPU <- wceGPU(data, n_knots, cutoff, constrained = "R",
-               id = "patient", event = "event", start = "start",
-               stop = "stop", expos = "dose", verbosity=0)
+    # wce_model_GPU <- wceGPU(data, n_knots, cutoff, constrained = "R",
+    #            id = "patient", event = "event", start = "start",
+    #            stop = "stop", expos = "dose", verbosity=0)
 
 
-    wce_model_CPU <- WCE(data,analysis = "cox", nknots = n_knots, cutoff = cutoff, constrained = "R",
-           id = "patient", event = "event", start = "start",
-           stop = "stop", expos = "dose")
+    # wce_model_CPU <- WCE(data,analysis = "cox", nknots = n_knots, cutoff = cutoff, constrained = "R",
+    #        id = "patient", event = "event", start = "start",
+    #        stop = "stop", expos = "dose")
 
     wce_model_GPU_bootstraps <- wceGPU(data, n_knots, cutoff, constrained = "R",
                id = "patient", event = "event", start = "start",
@@ -99,14 +106,14 @@ for (i in 1:nrow(combinaisons_parameters)){
     exposed   <- rep(1, cutoff)
     non_exposed   <- rep(0, cutoff)
     
-    HR_result_CPU = HR.WCE(wce_model_CPU,vecnum = exposed, vecdenom= non_exposed)
+    # HR_result_CPU = HR.WCE(wce_model_CPU,vecnum = exposed, vecdenom= non_exposed)
 
-    HR_result_GPU = HR(wce_model_GPU,vecnum = exposed, vecdenom= non_exposed)
+    # HR_result_GPU = HR(wce_model_GPU,vecnum = exposed, vecdenom= non_exposed)
 
     HR_result_GPU_bootstraps = HR(wce_model_GPU_bootstraps,vecnum = exposed, vecdenom= non_exposed)
 
-    print(paste0("HR CPU :",HR_result_CPU))
-    print(paste0("HR GPU :",HR_result_GPU))
+    # print(paste0("HR CPU :",HR_result_CPU))
+    # print(paste0("HR GPU :",HR_result_GPU))
     print(paste0("HR GPU_bootstraps :",HR_result_GPU_bootstraps))
 
     # print(exp(sum(wce_model$WCEmat[1,])))
@@ -127,8 +134,8 @@ for (i in 1:nrow(combinaisons_parameters)){
     lower_WCE_function_result_GPU_bootstraps = c()
     higher_WCE_function_result_GPU_bootstraps = c()
 
-    GPU_result = c()
-    CPU_result = c()
+    # GPU_result = c()
+    # CPU_result = c()
 
 
     
@@ -142,45 +149,50 @@ for (i in 1:nrow(combinaisons_parameters)){
         higher_WCE_function_result_GPU_bootstraps = c(higher_WCE_function_result_GPU_bootstraps, quantiles[2])
 
 
-        GPU_result = c(GPU_result, wce_model_GPU$WCEmat[,t])
-        CPU_result = c(CPU_result, wce_model_CPU$WCEmat[,t])
+        # GPU_result = c(GPU_result, wce_model_GPU$WCEmat[,t])
+        # CPU_result = c(CPU_result, wce_model_CPU$WCEmat[,t])
 
 
     }
 
-    # print(result)
-    maximum  = max(max(target_WCE_function),max(GPU_result))
-    minimum = min(0,GPU_result)
+    # maximum  = max(max(target_WCE_function),max(GPU_result))
+    # minimum = min(0,GPU_result)
 
 
-    plot(1:cutoff,
-    target_WCE_function,
-    type="l",
-    col="red",
-    ylim = c(minimum - 0.2*minimum,maximum+0.2*maximum),
-    xlab = "temps (j)",
-    ylab = "WCE weight",
-    main = paste0("GPU ",weight_function," patients : ",n_patients," HR : ",HR_target,"nknots : ",n_knots)
-    )
-    lines(1:cutoff,GPU_result,col="black")
+    # plot(1:cutoff,
+    # target_WCE_function,
+    # type="l",
+    # col="red",
+    # ylim = c(minimum - 0.2*minimum,maximum+0.2*maximum),
+    # xlab = "temps (j)",
+    # ylab = "WCE weight",
+    # main = paste0("GPU ",weight_function," patients : ",n_patients," HR : ",HR_target,"nknots : ",n_knots)
+    # )
+    # lines(1:cutoff,GPU_result,col="black")
 
-    maximum  = max(max(target_WCE_function),max(CPU_result))
-    minimum = min(0,CPU_result)
+    # maximum  = max(max(target_WCE_function),max(CPU_result))
+    # minimum = min(0,CPU_result)
 
-    plot(1:cutoff,
-    target_WCE_function,
-    type="l",
-    col="red",
-    ylim = c(minimum - 0.2*minimum,maximum+0.2*maximum),
-    xlab = "temps (j)",
-    ylab = "WCE weight",
-    main = paste0("CPU ",weight_function," patients : ",n_patients," HR : ",HR_target,"nknots : ",n_knots)
-    )
-    lines(1:cutoff,CPU_result,col="black")
+    # plot(1:cutoff,
+    # target_WCE_function,
+    # type="l",
+    # col="red",
+    # ylim = c(minimum - 0.2*minimum,maximum+0.2*maximum),
+    # xlab = "temps (j)",
+    # ylab = "WCE weight",
+    # main = paste0("CPU ",weight_function," patients : ",n_patients," HR : ",HR_target,"nknots : ",n_knots)
+    # )
+    # lines(1:cutoff,CPU_result,col="black")
 
 
     maximum  = max(max(higher_WCE_function_result_GPU_bootstraps),max(target_WCE_function))
     minimum = min(0,lower_WCE_function_result_GPU_bootstraps)
+
+    file_name = paste0(weight_function," patients : ",n_patients," HR : ",HR_target," nknots : ",n_knots,".png")
+
+    file_path = file.path("../Simulation_results",expriment_name,file_name)
+
+    png(file_path, width = 800, height = 600)
 
     plot(1:cutoff,
     target_WCE_function,
@@ -189,11 +201,13 @@ for (i in 1:nrow(combinaisons_parameters)){
     ylim = c(minimum - 0.2*minimum,maximum+0.2*maximum),
     xlab = "time (days)",
     ylab = "WCE weight",
-    main = paste0("GPU with bootstraps ",weight_function," patients : ",n_patients," HR : ",HR_target,"nknots : ",n_knots)
+    main = paste0(weight_function," patients : ",n_patients," HR : ",HR_target," nknots : ",n_knots)
     )
     lines(1:cutoff,mean_WCE_function_result_GPU_bootstraps,col="black")
     lines(1:cutoff,lower_WCE_function_result_GPU_bootstraps,col ="black", lty ="dashed" )    
     lines(1:cutoff,higher_WCE_function_result_GPU_bootstraps,col ="black", lty ="dashed" )
+
+    dev.off()
 
 
 
@@ -205,8 +219,8 @@ for (i in 1:nrow(combinaisons_parameters)){
     HR_GPU_bootstraps_results = c(HR_GPU_bootstraps_results,HR_result_GPU_bootstraps[1])  
     HR_GPU_bootstraps_2_5_results = c(HR_GPU_bootstraps_2_5_results,HR_result_GPU_bootstraps[2]) 
     HR_GPU_bootstraps_97_5_results = c(HR_GPU_bootstraps_97_5_results,HR_result_GPU_bootstraps[3])  
-    HR_GPU_results = c(HR_GPU_results,HR_result_GPU)
-    HR_CPU_resutls = c(HR_CPU_resutls,HR_result_CPU)
+    # HR_GPU_results = c(HR_GPU_results,HR_result_GPU)
+    # HR_CPU_resutls = c(HR_CPU_resutls,HR_result_CPU)
 
 
 
@@ -218,20 +232,13 @@ result_dict = list("weight_function"= weight_function_results,
                    "HR_calculated_GPU_bootstraps"= HR_GPU_bootstraps_results,
                    "HR_calculated_GPU_bootstraps_2_5"= HR_GPU_bootstraps_2_5_results,
                    "HR_calculated_GPU_bootstraps_97_5"= HR_GPU_bootstraps_97_5_results,
-                   "HR_GPU" = HR_GPU_results,
-                   "HR_CPU" = HR_CPU_resutls
+                #    "HR_GPU" = HR_GPU_results,
+                #    "HR_CPU" = HR_CPU_resutls
                    )
 
 }
 
 result_dict_path = file.path("../Simulation_results",expriment_name)
-
-
-
-
-if (!dir.exists(result_dict_path)){
-dir.create(result_dict_path)
-} 
 
 file_result_name = paste0("analyzed_",expriment_name,".csv")
 file_result_path = file.path(result_dict_path,file_result_name)
