b4e1277084a4cfee60c82fe41a81f453a1303be7
Antoine Poirot-Bourdain <antoine.poirot.bourdain@gmail.com>
2024-04-22 17:22:48 +0200

    old benchark permalgo OK

--- simulations/R/benchmark_old_permalgo.R
+++ simulations/R/benchmark_old_permalgo.R
@@ -15,9 +15,9 @@ source("simulation_and_analysis.R")
 
 
 
-n_patients_list = c(100,100,200,5000,1000,2000,5000,10000,20000,50000,100000)
+n_patients_list = c(100,200,500,1000,2000,5000,10000)#c(100,100,200,5000,1000,2000,5000,10000,20000,50000,100000)
 
-max_time = 365
+observation_time = 365
 doses = c(1,1.5,2,2.5,3)
 HR_target = 1.5
 constraint = "R"
@@ -27,9 +27,8 @@ n_bootstraps = 1000
 cutoff = 180
 n_knots = 1
 
-experiment_name <- "2024-04-22 : benchmark CPU old permalgo"
+experiment_name <- "2024-04-22 : benchmark CPU old permalgo_19"
 
-computation_type <- "CPU old permalgo"
 
 result_dict_path = file.path("../Simulation_results",experiment_name)
 
@@ -47,8 +46,11 @@ file_result_path = file.path(result_df_path,file_result_name)
 number_conditions = length(n_patients_list)
 
 df_results = data.frame(n_patients = integer(number_conditions),
-                       simulation_time = integer(number_conditions)
+                       simulation_time = integer(number_conditions),
+                       type = integer(number_conditions)
                        )
+
+print(df_results)
 
 number_analyzed_conditions = 0
 
@@ -56,25 +58,18 @@ number_analyzed_conditions = 0
 for (n_patients in n_patients_list){
 
     number_analyzed_conditions = number_analyzed_conditions + 1
-    df_results$computation_type[number_analyzed_conditions] = computation_type
     df_results$n_patients[number_analyzed_conditions] = n_patients
 
-
     start_simulation_time = Sys.time()
-    dataset  <- generate_dataset_batch(
-            doses = doses,
-            observation_time = max_time,
-            n_patients = n_patients,
-            scenario = scenario,
-            cutoff = cutoff,
-            HR_target = HR_target,
-            batchsize = 10000000     
-        )
+    dataset = genrate_dataset_process(doses,observation_time, n_patients,scenario,cutoff,HR_target)
+
+    print("dataset")
     end_simulation_time = Sys.time()
     elapsed_simulation_time = end_simulation_time - start_simulation_time
     df_results$simulation_time[number_analyzed_conditions] = elapsed_simulation_time
     
     write.csv(df_results, file_result_path)
+    print(df_results)
 
 
 }
