8a0d144ce14a907fb95c58a6173cc31a89cdbad8
Antoine Poirot-Bourdain <antoine.poirot.bourdain@gmail.com>
2025-11-27 17:07:50 +0100

    fix typo imprt simulations

--- python/survivalgpu/__init__.py
+++ python/survivalgpu/__init__.py
@@ -2,13 +2,7 @@ import torch
 
 from . import optimizers, utils
 from .coxph import CoxPHSurvivalAnalysis, coxph_numpy, coxph_R
-from .simulation import (
-    WCE_permalgo,
-    get_dataset,
-    matching_algo,
-    simulate_dataset,
-    simulate_for_experiment,
-)
+from .simulations import simulate_for_experiment
 from .utils import device, float32, int32, int64, use_cuda
 from .wce import WCESurvivalAnalysis, wce_numpy, wce_R
 
